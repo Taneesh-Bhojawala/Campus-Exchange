2814015e343983587d6213479e3c767e60610546
Taneesh Bhojawala <taneeshbhojawala@gmail.com>
2025-11-21 22:56:08 +0530

    Completed the PendingSignup.java in model dir

--- src/main/java/com/campus/exchange/model/PendingSignup.java
+++ src/main/java/com/campus/exchange/model/PendingSignup.java
@@ -1,4 +1,22 @@
 package com.campus.exchange.model;
 
-public class PendingSignup {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor              //lombok annotation that creates the default constructor
+@AllArgsConstructor             //lombok annotation that creates the all parameter constructor.
+public class PendingSignup
+{
+    private String mail;        //email of the user
+
+    private String name;        //real name of the user
+    private String hashPassword;//hashed password for login
+    private String hostelNumber;//hostel number of the user
+    private String gender;      //gender of the user
+    private String college;     //college of the user
+
+    private String otp;         //otp that is generated for the signup
+    private long expiresAt;     //the time when otp will expire (now + 5 minutes)
 }
