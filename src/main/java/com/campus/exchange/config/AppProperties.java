f61b20a4b359326d7ffd1e03abc407409396775c
Abhinav Bhatia <abhinavbhatia401@gmail.com>
2025-11-21 22:27:18 +0530

    AppProperties.java created

--- src/main/java/com/campus/exchange/config/AppProperties.java
+++ src/main/java/com/campus/exchange/config/AppProperties.java
@@ -1,4 +1,42 @@
 package com.campus.exchange.config;
-//hello
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/*
+* Loads custom configuration values from application.properties using the prefix "app".
+ */
+
+@Component
+@ConfigurationProperties(prefix = "app")
 public class AppProperties {
+
+    /*
+    Path to folders where all data files are stored (acting as database)
+     */
+    private String dataFolder;
+
+    /*
+    Directory where images uploaded by users will be stored (for item listings)
+     */
+    private String uploadDir;
+
+    /*
+    getters and setters
+     */
+    public String getDataFolder() {
+        return dataFolder;
+    }
+
+    public String getUploadDir() {
+        return uploadDir;
+    }
+
+    public void setDataFolder(String dataFolder) {
+        this.dataFolder = dataFolder;
+    }
+
+    public void setUploadDir(String uploadDir) {
+        this.uploadDir = uploadDir;
+    }
 }
