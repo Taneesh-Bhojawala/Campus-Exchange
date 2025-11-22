73cd1e279b1739a691e4f1068593d271a94a5da5
Abhinav Bhatia <abhinavbhatia401@gmail.com>
2025-11-22 18:44:50 +0530

    UserRepository.java created

--- src/main/java/com/campus/exchange/CampusExchangeApplication.java
+++ src/main/java/com/campus/exchange/CampusExchangeApplication.java
@@ -2,8 +2,10 @@ package com.campus.exchange;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class CampusExchangeApplication {
 
 	public static void main(String[] args) {
