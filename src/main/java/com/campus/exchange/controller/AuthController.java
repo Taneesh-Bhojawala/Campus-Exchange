8009893ded53dc308e9075d1481646fe39ca4136
Taneesh Bhojawala <taneeshbhojawala@gmail.com>
2025-11-22 21:43:16 +0530

    implemented AuthController

--- src/main/java/com/campus/exchange/controller/AuthController.java
+++ src/main/java/com/campus/exchange/controller/AuthController.java
@@ -1,4 +1,49 @@
 package com.campus.exchange.controller;
 
-public class AuthController {
+import com.campus.exchange.dto.SignupRequest;
+import com.campus.exchange.dto.VerifyOtpRequest;
+import com.campus.exchange.model.User;
+import com.campus.exchange.service.AuthService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController
+{
+    private AuthService authService;
+    public AuthController(AuthService authService)
+    {
+        this.authService = authService;
+    }
+
+    @PostMapping("/signup-request")
+    public ResponseEntity<?> signupRequest(@RequestBody SignupRequest signupRequest)
+    {
+        try
+        {
+            String result = authService.signupRequest(signupRequest);
+            return ResponseEntity.ok().body(result);
+        }
+        catch (Exception e)
+        {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    public ResponseEntity<?> verifyOtp(@RequestBody VerifyOtpRequest verifyOtpRequest)
+    {
+        try
+        {
+            User user = authService.verifyOtp(verifyOtpRequest);
+            return ResponseEntity.ok().body(user);
+        }
+        catch (Exception e)
+        {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
 }
