fbf1aaeade1d083bba7480f600c693f721a126e1
Taneesh Bhojawala <taneeshbhojawala@gmail.com>
2025-11-24 23:17:47 +0530

    changes in few files related to claims

--- src/main/java/com/campus/exchange/model/Claim.java
+++ src/main/java/com/campus/exchange/model/Claim.java
@@ -7,10 +7,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Claim {
-    private String claimID;//assign id to a claim which was requested
-    private String itemID; //stores the id of the item to be claimed
-    private String claimerID;//stores the id of claimer
-    private String listerID;//stores the id of person listing the item
+    private String claimId;//assign id to a claim which was requested
+    private String itemId; //stores the id of the item to be claimed
+    private String claimerId;//stores the id of claimer
+    private String listerId;//stores the id of person listing the item
     private String status;//stores the state of object i.e. ACCEPTED, WAITING, REJECTED
     private long createdAt;//stores at what time is the object claim created
 }
